1fb25de023ed2724926972fd1d6c5b01334c2a9b
Firas KSONTINI <firas.ksontini@gmail.com>
2017-01-04 19:06:52 +0100

    Add Projects and Files.

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Data
+namespace GUI
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
