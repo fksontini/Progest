d066a15e44b6db12cbd28a14f37a963117ed6151
Firas KSONTINI <firas.ksontini@gmail.com>
2017-01-04 19:37:33 +0100

    configure Classes and Connection String

---
+++ Data/Context.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Context:DbContext
+    {
+        public Context():base("Name=MyProject")
+        {
+               
+        }
+        //public DbSet<your entity> entity name{ get; set; }
+     
+
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+
+        //    //modelBuilder.Conventions.Add(new DateTime2Convention());
+        //    //modelBuilder.Configurations.Add(new ProductConfiguration());
+        //}
+
+    }
+    //public class MyFinanceContextInitialiser : DropCreateDatabaseIfModelChanges<Context>
+    //{
+    //    protected override void Seed(Context context)
+    //    {
+    //        Product p = new Product();
+    //        p.Name = "def prod";
+    //        p.Description = "def desc";
+    //        p.fullprice = new FullPrice { Price = 15, Quantity = 17 };
+    //        p.DateProd = DateTime.Now;
+    //        context.product.Add(p);
+    //        context.SaveChanges();
+
+    //    }
+
+    //}
+
+
+}
---
+++ Data/CustomConvention/DateTime2Convention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.CustomConvention
+{
+    public class DateTime2Convention : Convention
+    {
+
+        // convention of time.
+        public DateTime2Convention()
+        {
+            this.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
+        }
+    }
+}
