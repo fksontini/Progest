cb3d1fd4cedf4c81481b074f9df227c0fcec11d9
firas.ksontini <firas.ksontini@esprit.tn>
2017-01-11 13:07:31 +0100

    add classes [Person, with their child] [Discussion]

---
+++ Domain/SimplePerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SimplePerson:Person
+    {
+        public string Bibiographe { get; set; }
+    }
+}
